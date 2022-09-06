b936ba07461b4f34e67e93fdf6ea2ed5cdd67a8b
Artem Lysechko <artem.lysechko@synebo.io>
2022-09-06 15:11:24 +0300

    HW5

---
+++ HomeTask_5/script.js
@@ -0,0 +1,56 @@
+const supportedActions = { 
+  '+': (a, b) => a + b,
+  '-': (a, b) => a - b,
+  '*': (a, b) => a * b,
+  '/': (a, b) => a / b
+}
+
+const action = getAction('Choose an action from "+", "-", "/", "*"');
+const operandsArr = getNumbers('Enter valid numbers divided by comma (min: 2)');
+const result = calculateResult(operandsArr, action);
+alert(`Result: ${result}`);
+
+
+function getNumbers(label){
+  let input = ''; 
+  do {
+    input = prompt(label, input);
+    console.log('input', input);
+  } while(!isValidNumbers(input));
+  
+  return input.split(',').map((elem) => { return +elem });
+}
+
+function getAction(label){
+  let input = '+';
+  do {
+    input = prompt(label, input);
+  } while(!isValidAction(input));
+
+  return input;
+}
+
+function isValidNumbers(input){
+  if(input == null || input.trim() === ''){
+    return false;
+  }
+  const rawArr = input.split(',');
+  if(rawArr.length < 2){
+    return false;
+  }
+  return rawArr.every(isValidNum);
+}
+
+function isValidNum(input){
+  return input.trim() !== '' && !isNaN(+input);
+}
+
+function isValidAction(input){
+  return input in supportedActions;
+}
+
+function calculateResult(numList, operator){
+  return numList.reduce(
+    (sum, current) => supportedActions[operator](sum, current)
+  );
+}
