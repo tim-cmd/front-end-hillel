0814906052413289ba7e967bcaa05f4716d47cde
Artem Lysechko <artem.lysechko@synebo.io>
2022-09-09 09:45:55 +0300

    HW6

---
+++ HomeTask_6/script.js
@@ -0,0 +1,41 @@
+const students = [
+  {
+      id:10,
+      name: 'John Smith',
+      marks: [10, 8, 6, 9, 8, 7 ]
+  },
+  {
+      id:11,
+      name: 'John Doe',
+      marks: [ 9, 8, 7, 6, 7 ]
+  },
+  {
+      id:12,
+      name: 'Thomas Anderson',
+      marks: [6, 7, 10, 8 ]
+  },
+  {
+      id:13,
+      name: 'Jean-Baptiste Emanuel Zorg',
+      marks: [10, 9, 8, 9 ]
+  }
+];
+
+function calculateStudentAverageMark(student){
+  return calculateAverage(student.marks);
+}
+
+function calculateGroupAverageMark(studentsList){
+  let allMarks = []
+  studentsList.forEach(student => allMarks = allMarks.concat(student.marks));
+  // const allMarks = studentsList.reduce((accum, current) => accum = accum.concat(current.marks),[]);
+  return calculateAverage(allMarks);
+}
+
+/**
+ * @param {number[]} numsArr 
+ * @returns {number} array avg
+ */
+function calculateAverage(numsArr){
+  return numsArr.reduce((accum, current) => accum + current) / numsArr.length;
+}
